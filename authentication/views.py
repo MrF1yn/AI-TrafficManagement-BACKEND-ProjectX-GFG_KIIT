a991c6abed37c802445d4d8ff0cce434dd9fe386
DibyajyotiDey <dibyajyoti49@outlook.com>
2024-06-12 17:08:57 +0530

    shifted to django rest framework and django allauth, made routes for video upload, get and delete

---
+++ authentication/views.py
@@ -0,0 +1,18 @@
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+from dj_rest_auth.registration.views import SocialLoginView
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = "http://127.0.0.1:3000/"
+    client_class = OAuth2Client
+
+
+
---
+++ restapi/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+import os
+import shutil
+
+from django.contrib import admin
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+# Create your views here.
+@api_view(['POST'])
+def upload_video(request):
+    traffic_lights = []
+
+    if request.FILES['video_file']:
+        video_files = request.FILES.getlist('video_file')
+        fs = FileSystemStorage()
+        video_dir = 'uploaded_videos/' + request.user.username + '/'
+        if fs.exists(video_dir) and len(os.listdir(video_dir)) > 3:
+            # if fs.exists(video_dir):
+            #     shutil.rmtree(video_dir)
+            return HttpResponse("FILE UPLOAD LIMIT REACHED", status=400)
+
+        uploaded_file_urls = []
+        for video_file in video_files:
+            filename = fs.save(video_dir + video_file.name, video_file)
+            print(video_file.name)
+            uploaded_file_urls.append(fs.path(filename))
+            print("Uploaded successfully")
+        return HttpResponse("SUCCESS")
+    return HttpResponse("Incomplete Data, No Video File", status=400)
+
+
+@api_view(['GET'])
+def clear_uploads(request):
+    fs = FileSystemStorage()
+    video_dir = 'uploaded_videos/' + request.user.username
+    if fs.exists(video_dir):
+        shutil.rmtree(video_dir)
+    return HttpResponse("SUCCESS")
+
+
+@api_view(['GET'])
+def get_uploads(request):
+    fs = FileSystemStorage()
+    video_dir = 'uploaded_videos/' + request.user.username + '/'
+    if fs.exists(video_dir):
+        [folders, files] = fs.listdir(video_dir)
+        return Response(files)
+    return Response([])
